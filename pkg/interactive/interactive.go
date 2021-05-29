b41018fb2e525213d2bc56f5c7314038033e8aaa
alyakimenko <urlucidfall@gmail.com>
2021-05-29 12:47:59 +0300

    justify linter

--- pkg/interactive/interactive.go
+++ pkg/interactive/interactive.go
@@ -2,7 +2,7 @@ package interactive
 
 func SSHInteractive(user, instruction string, questions []string, echos []bool) (answers []string, err error) {
 	answers = make([]string, len(questions))
-	for n, _ := range questions {
+	for n := range questions {
 		answers[n] = "root"
 	}
 
